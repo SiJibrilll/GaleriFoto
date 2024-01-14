eca15246782d8f25b8e5761bb3924f3fa517744e
SiJibrilll <109641836+SiJibrilll@users.noreply.github.com>
2024-01-14 20:49:08 +0700

    implemented infinite scrolling

--- resources/views/livewire/home-livewire.blade.php
+++ resources/views/livewire/home-livewire.blade.php
@@ -4,8 +4,12 @@
     <h1>Livewire</h1>
 
     @for ($i = 0; $i < count($posts); $i++) 
+
+        @if ($i + 7 == count($posts))
+            <div x-intersect='$wire.loadMore'></div>
+        @endif
         <img class="max-h-72 w-48 object-cover" src="{{asset("storage/images/postImage/" . $posts[$i][1])}}" alt="Image">
     @endfor
 
-    <button wire:click='loadMore'>Load</button>
+    {{-- <button wire:click='loadMore'>Load</button> --}}
 </div>
