37e1616b8368aae2794653a2b41f176d7e8e833e
SiJibrilll <109641836+SiJibrilll@users.noreply.github.com>
2024-01-15 12:38:16 +0700

    implement comment panel on front end

---
+++ resources/views/livewire/create-comments.blade.php
@@ -0,0 +1,9 @@
+<ul class="space-y-4">
+    {{-- The best athlete wants his opponent at his best. --}}
+
+    @for ($i = 0; $i < 20; $i++)
+    <li class="border-b border-gray-200">
+        <p class="text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </li>
+    @endfor
+</ul>
--- resources/views/show-post.blade.php
+++ resources/views/show-post.blade.php
@@ -12,4 +12,52 @@
     <h1> {{$post->title}} </h1>
     <h1> {{$post->description}} </h1>
 
+    <div class="modal-bg hidden fixed inset-0 bg-black bg-opacity-50 transition-opacity duration-300 ease-in-out" aria-hidden="true"></div>
+
+    <div class="comment-popup hidden fixed bottom-0 inset-x-0 sm:inset-0 sm:flex sm:items-center sm:justify-center transition-all duration-300 ease-in-out transform translate-y-full opacity-0">
+      <div class="bg-white rounded-s-xl rounded-e-xl shadow-md overflow-y-scroll w-full max-h-[75vh]">
+        <div class="p-4 scroll-smooth overflow-y-scroll"> 
+            <h1>Comments </h1>
+            @livewire('create-comments')
+        </div>
+      </div>
+    </div>
+    
+
+      <button onclick="showModal()">OPEN</button>
+
+      <script>
+        const modal = document.querySelector('.comment-popup');
+        const backdrop = document.querySelector('.modal-bg');
+        const body = document.body;
+
+function showModal() {
+    
+    backdrop.classList.remove('hidden');
+    modal.classList.remove('hidden');
+    setTimeout(() => {
+        body.classList.add('overflow-hidden'); // Disable scrolling on the body
+        backdrop.classList.remove('opacity-0');
+        modal.classList.remove('opacity-0');
+        modal.classList.remove('translate-y-full');
+        
+  }, 100);
+}
+
+function hideModal() {
+body.classList.remove('overflow-hidden'); // Re-enable scrolling on the body
+  backdrop.classList.add('opacity-0');
+  backdrop.classList.add('hidden');
+  modal.classList.add('opacity-0');
+  modal.classList.add('translate-y-full');
+}
+
+modal.addEventListener('click', (event) => {
+  // Prevent clicks inside the modal from closing it
+  event.stopPropagation();
+});
+
+backdrop.addEventListener('click', hideModal);
+    </script>
+
 </x-layout>
