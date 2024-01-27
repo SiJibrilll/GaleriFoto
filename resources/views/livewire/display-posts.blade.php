731b2fb3f2067c206c248b8ee75ddc3eaac5cd24
SiJibrilll <109641836+SiJibrilll@users.noreply.github.com>
2024-01-27 11:37:38 +0700

    oopsies

--- resources/views/livewire/display-posts.blade.php
+++ resources/views/livewire/display-posts.blade.php
@@ -1,39 +1,36 @@
 <div>
+    
     {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}    
         {{-- // TODO we need to find a way to decide how much collumns we need for each screens, bigger screens having more collumns  --}}
-    <div class="mx-2 gap-2 columns-2 sm:columns-4">
-        @for ($i = 0; $i < count($posts); $i++) 
+    
+        <div class="grid">
+            @for ($i = 0; $i < count($posts); $i++) 
 
-            {{-- this div is used to trigger the infinite scrolling --}}
-            @if ($i + 7 == count($posts))
-                <div x-intersect='$wire.loadMore'></div>
-            @endif
-
-            {{-- post cards --}}
-            <div class="mb-2 max-w-44 overflow-hidden rounded-2xl main-{{$posts[$i][0]}}" onclick="window.location.href = '/posts/show/{{$posts[$i][0]}}';">
-                {{-- post thumbnail --}}
-                <img class="hidden relative  object-cover brightness-95" src="{{asset("storage/images/postImage/" . $posts[$i][1])}}" alt="Image" onload="loaded({{$posts[$i][0]}})">
+            <div class="grid-item main-{{$posts[$i][0]}}">
+                <img class=" object-cover brightness-95" src="{{asset("storage/images/postImage/" . $posts[$i][1])}}" alt="Image" onload="">
                 
-                {{-- placeholder skeleton loader --}}
-                <div id="loader" class="relative space-y-8 animate-pulse  rtl:space-x-reverse">
-                    <div class="flex items-center justify-center w-full h-48 bg-gray-300 rounded dark:bg-gray-700">
-                        <svg class="w-10 h-10 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
-                            <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z"/>
-                        </svg>
-                    </div> 
-                    <span class="sr-only">Loading...</span>
-                </div>
-            
-            
             </div>
-        @endfor
-    </div>
-    <script> // script to show the image once it loads
-        function loaded($key) {
-            let main = document.querySelector('.main-' + $key);           
+            @endfor
+          </div>
 
-            main.querySelector('img').classList.remove('hidden');
-            main.querySelector('div').classList.add('hidden')
-        }
+
+    <script src="https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js"></script>
+    <script>
+    // script to show the image once it loads
+    function loaded($key) {
+        let main = document.querySelector('.main-' + $key);           
+
+        main.querySelector('img').classList.remove('hidden');
+        main.querySelector('div').classList.add('hidden')
+    }
+
+    var elem = document.querySelector('.grid');
+    var msnry = new Masonry( elem, {
+    // options
+    itemSelector: '.grid-item',
+    columnWidth: 600
+    });  
+    
+    
     </script>
 </div>
