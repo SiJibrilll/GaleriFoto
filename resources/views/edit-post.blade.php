2cb42d56d83cba5974ac74dce78fc503792c57a0
SiJibrilll <109641836+SiJibrilll@users.noreply.github.com>
2024-01-30 09:12:04 +0700

    oppsies

--- resources/views/edit-post.blade.php
+++ resources/views/edit-post.blade.php
@@ -33,6 +33,9 @@
         <button type="submit" form="myform" class="w-24 h-9 bg-gray-800 text-white text-xs rounded-3xl font-normal font-['Poppins']">Submit</button>
     </div>
 
+    {{-- {{dd($post->images->map(function ($image) {
+        return asset("storage/images/postImage/") . $image->image;
+    })->all())}} --}}
 
     <script>
         // Register the plugin
@@ -48,6 +51,7 @@
 
         FilePond.setOptions({
             server: {
+                load: '/',
                 process: '/tmp-image/create',
                 revert: '/tmp-image/delete',
                 headers: {
@@ -55,6 +59,15 @@
                 },
                 allowMultiple: true,
             },
+            files: [
+                {
+                source:  'folder-65b84d3679fdd3.64021550/image-65b84d3679fd94.44546420',
+                options: {
+                    type: 'local',
+                    encodeFilename: false
+                    }
+                }
+            ],
         });
 
         let images = @json($post->images);
