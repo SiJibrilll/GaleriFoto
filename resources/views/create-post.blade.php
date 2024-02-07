cf4854f0b969ff6a502d3d27560549a3281a3dfb
SiJibrilll <109641836+SiJibrilll@users.noreply.github.com>
2024-02-07 21:15:45 +0700

    create post fully responsive
    now create post supports all screen dimensions including tablets

--- resources/views/create-post.blade.php
+++ resources/views/create-post.blade.php
@@ -6,36 +6,40 @@
 
     <form class="mt-12 mx-2"  action="/posts/store" method="POST" enctype="multipart/form-data">
         @csrf
+        <div class="flex flex-col md:flex-row xl:mx-52 md:gap-16">
 
-        <input class="filepond" type="file" name='image[]' multiple credits='false' >
-
-        @error('image')
-            <small class="text-red-500 text-xs mt-1">{{ $message }}</small>
-        @enderror
-
-
-        <h1 class="text-black font-normal font-['Poppins'] mt-9">Title</h1>
-        <div class="flex">
-            <textarea class="flex-grow w-full rounded-md border border-zinc-200" placeholder="Add a title" name="title"></textarea>
-        </div>
-
-        @error('title')
-            <small class="text-red-500 text-xs mt-1">{{ $message }}</small>
-        @enderror
-
-        <h1 class="text-black font-normal font-['Poppins'] mt-4" >Description</h1>
-        <div class="flex">
-            <textarea class="flex-grow w-full rounded-md border border-zinc-200" placeholder="Add more details" name="description"></textarea>
-        </div>
-
-        <h1 class="text-black font-normal font-['Poppins'] mt-4">Tags</h1>
-        <div class="flex">
-            <textarea class="flex-grow w-full rounded-md border border-zinc-200" name="tags" placeholder="Seperate tags with spaces"></textarea>
-        </div>
-
-        <div class="flex justify-end mt-6 mb-28">
-            <button type="submit" class="w-24 h-9 bg-gray-800 text-white text-xs rounded-3xl font-normal font-['Poppins']">Submit</button>
-        </div>
+            <div class="w-full md:mt-1">
+                <input class="filepond" type="file" name='image[]' multiple credits='false' >                
+                @error('image')
+                    <small class="text-red-500 text-xs mt-1">{{ $message }}</small>
+                @enderror
+            </div>
+    
+            <div class="flex flex-col w-full">
+                <h1 class="text-black font-normal font-['Poppins'] mt-9 md:mt-0">Title</h1>
+                <div class="flex">
+                    <textarea class="flex-grow w-full rounded-xl p-2 border border-zinc-200 resize-none" placeholder="Add a title" name="title"></textarea>
+                </div>
+        
+                @error('title')
+                    <small class="text-red-500 text-xs mt-1">{{ $message }}</small>
+                @enderror
+        
+                <h1 class="text-black font-normal font-['Poppins'] mt-4 md:mt-8" >Description</h1>
+                <div class="flex">
+                    <textarea class="flex-grow w-full rounded-xl p-2 border border-zinc-200 resize-none md:h-52" placeholder="Add more details" name="description"></textarea>
+                </div>
+        
+                <h1 class="text-black font-normal font-['Poppins'] mt-4 md:mt-8">Tags</h1>
+                <div class="flex">
+                    <textarea class="flex-grow w-full rounded-xl p-2 border border-zinc-200 resize-none" name="tags" placeholder="Seperate tags with spaces"></textarea>
+                </div>
+        
+                <div class="flex justify-end mt-6 mb-28 md:mt-8">
+                    <button type="submit" class="w-24 h-9 bg-gray-800 text-white text-xs rounded-3xl font-normal font-['Poppins']">Submit</button>
+                </div>
+            </div>
+        </div>    
     </form>
 
     <script src="https://unpkg.com/filepond-plugin-image-preview/dist/filepond-plugin-image-preview.js"></script>
