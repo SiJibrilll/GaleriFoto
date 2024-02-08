32a3f3e175146234afdea8e1f8d1fee99eb55f31
SiJibrilll <109641836+SiJibrilll@users.noreply.github.com>
2024-02-09 06:20:11 +0700

    edit post responsive
    edit post now can handle mobile, widescreen, and desktop

--- resources/views/edit-post.blade.php
+++ resources/views/edit-post.blade.php
@@ -6,36 +6,44 @@
     <form id="myform" class="mt-12 mx-2" action="/posts/update/{{$post->id}}" method="POST" enctype="multipart/form-data">
         @csrf
 
-        <input class="filepond" type="file" name='image[]' multiple credits='false'>
+        <div class="flex flex-col md:flex-row xl:mx-52 md:gap-16">
 
-        @error('image')
-            <small class="text-red-500 text-xs mt-1">{{ $message }}</small>
-        @enderror
+                <div class="w-full md:mt-1">
+
+                    <input class="filepond" type="file" name='image[]' multiple credits='false'>
+
+                    @error('image')
+                        <small class="text-red-500 text-xs mt-1">{{ $message }}</small>
+                    @enderror
+                </div>
+
+                <div class="flex flex-col w-full">
+
+                    <h1 class="text-black font-normal font-['Poppins'] mt-9">Title</h1>
+                    <div class="flex">
+                        <textarea placeholder="Add a title" class="flex-grow w-full rounded-md border border-zinc-200" name="title">{{null == old('title')? $post->title : old('title')}}</textarea>
+                    </div>
+
+                    @error('title')
+                        <small class="text-red-500 text-xs mt-1">{{ $message }}</small>
+                    @enderror
 
 
-        <h1 class="text-black font-normal font-['Poppins'] mt-9">Title</h1>
-        <div class="flex">
-            <textarea placeholder="Add a title" class="flex-grow w-full rounded-md border border-zinc-200" name="title">{{null == old('title')? $post->title : old('title')}}</textarea>
-        </div>
+                    <h1 class="text-black font-normal font-['Poppins'] mt-4">Description</h1>
+                    <div class="flex">
+                        <textarea placeholder="Add more details" class="flex-grow w-full rounded-md border border-zinc-200" name="description">{{null == old('description')? $post->description : old('description')}}</textarea>
+                    </div>
 
-        @error('title')
-            <small class="text-red-500 text-xs mt-1">{{ $message }}</small>
-        @enderror
-
-
-        <h1 class="text-black font-normal font-['Poppins'] mt-4">Description</h1>
-        <div class="flex">
-            <textarea placeholder="Add more details" class="flex-grow w-full rounded-md border border-zinc-200" name="description">{{null == old('description')? $post->description : old('description')}}</textarea>
-        </div>
-
-        <h1 class="text-black font-normal font-['Poppins'] mt-4">Tags</h1>
-        <div class="flex">
-            <textarea placeholder="Seperate tags with spaces" class="flex-grow w-full rounded-md border border-zinc-200" name="tags">@if(old('tags') != null){{old('tags')}}@else @foreach ($post->tags as $tag){{$tag->name}}@endforeach @endif</textarea>
-        </div>
+                    <h1 class="text-black font-normal font-['Poppins'] mt-4">Tags</h1>
+                    <div class="flex">
+                        <textarea placeholder="Seperate tags with spaces" class="flex-grow w-full rounded-md border border-zinc-200" name="tags">@if(old('tags') != null){{old('tags')}}@else @foreach ($post->tags as $tag){{$tag->name}}@endforeach @endif</textarea>
+                    </div>
+                </div>
+            </div>
 
     </form>
 
-    <div class="flex justify-end mt-6 mx-2 items-center mb-28">
+    <div class="flex justify-end mt-6 mx-2 items-center mb-28 xl:mr-52 xl:p-2">
         <form action="/posts/delete/{{$post->id}}" method="POST">
             @csrf
             <button type="submit" class="mr-4 w-24 h-9 bg-red-500 text-white text-xs rounded-3xl font-normal font-['Poppins']">Delete post</button>
