5f6c4a81eb3a101771e48faf91028afc67bb147f
SiJibrilll <109641836+SiJibrilll@users.noreply.github.com>
2024-02-09 16:39:51 +0700

    buttons hoverable

--- resources/views/livewire/profile-livewire.blade.php
+++ resources/views/livewire/profile-livewire.blade.php
@@ -1,10 +1,10 @@
 <div>
     <div class="flex w-full justify-center items-center gap-8 mb-11">
-        <div wire:click='select("posts")' class="{{$selected == 'posts' ? 'border-b border-black' : ''}} justify-center items-center">
+        <div style="cursor: pointer;" wire:click='select("posts")' class="{{$selected == 'posts' ? 'border-b border-black' : ''}} justify-center items-center">
             <h1 class="text-black text-xs font-normal font-['Poppins']">Posts</h1>
         </div>
         @if ($user == Auth()->user()->id)
-            <div wire:click='select("albums")' class="{{$selected == 'albums' ? 'border-b border-black' : ''}}  justify-center items-center">
+            <div style="cursor: pointer;" wire:click='select("albums")' class="{{$selected == 'albums' ? 'border-b border-black' : ''}}  justify-center items-center">
                 <h1 class="text-black text-xs font-normal font-['Poppins']">Albums</h1>
             </div>            
         @endif
